ca1c277637a9a8e87de00dd03c167d4865cac782
= <yasithpiyarathne@gmail.com>
2023-06-17 20:35:20 +0530

    wraped up

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from code_blocks import grant_access_from_partial
+import partials0 as core
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def grant_access():
+    successful, unsuccessful = grant_access_from_partial(core)
+    print(successful, unsuccessful)
+    return render_template('access_template.html', successful=successful, unsuccessful=unsuccessful)
