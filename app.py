d33d89970873eeaf61786d172dccc5c1d0f69924
= <yasithpiyarathne@gmail.com>
2023-06-17 21:37:29 +0530

    removed everything related to partials

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.errors import HttpError
 
-from code_blocks import grant_access_from_partial
-import partials0 as core
+import config
+import code_blocks
+import permissions
+import sheets
 
 app = Flask(__name__)
 
 @app.route('/')
 def grant_access():
-    successful, unsuccessful = grant_access_from_partial(core)
-    print(successful, unsuccessful)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(config.SERVICE_KEY_PATH, [config.SCOPE])
+    sheet = build('sheets', 'v4', credentials=credentials)
+    drive = build('drive', 'v3', credentials=credentials)
+
+    submitted_users = code_blocks.get_submited_users(sheets.get_sheet_data(sheet, config.DATA_SHEET_ID, config.DATA_SHEET_TITLE))
+    print(submitted_users)
+    users_with_access = code_blocks.get_readable_users(permissions.list_permissions(drive, config.SHARED_FOLDER_ID))
+    print(users_with_access)
+
+    users_without_access = [user for user in submitted_users if user not in users_with_access]
+    print(users_without_access)
+    successful = list()
+    unsuccessful = list()
+
+    for user in users_without_access:
+        try:
+            permissions.create_permission(user, drive, config.SHARED_FOLDER_ID)
+            successful.append(user)
+        except HttpError as err:
+            unsuccessful.append((user, err.reason))
     return render_template('access_template.html', successful=successful, unsuccessful=unsuccessful)
--- code_blocks.py
+++ code_blocks.py
@@ -1,14 +1,3 @@
-from oauth2client.service_account import ServiceAccountCredentials 
-from googleapiclient.errors import HttpError
-from googleapiclient.discovery import build
-
-import config
-
-credentials = ServiceAccountCredentials.from_json_keyfile_name(config.SERVICE_KEY_PATH, [config.SCOPE])
-
-drive = build('drive', 'v3', credentials=credentials)
-sheet = build('sheets', 'v4', credentials=credentials)
-
 def get_readable_users(data_list):
     email_mapper = lambda user:user['emailAddress']
     reading_filter = lambda user:user['role']=='reader'
@@ -19,22 +8,22 @@ def get_submited_users(data_list):
     email_mapper = lambda user:user['email']
     return list(map(email_mapper, data_list))
 
-def grant_access_from_partial(core):
-    submitted_users = core.get_submited_users()
-    print(submitted_users)
-    users_with_access = core.get_readable_users()
-    print(users_with_access)
-    users_without_access = [user for user in submitted_users if user not in users_with_access]
-    print(users_without_access)
+# def grant_access_from_partial(core):
+#     submitted_users = core.get_submited_users()
+#     print(submitted_users)
+#     users_with_access = core.get_readable_users()
+#     print(users_with_access)
+#     users_without_access = [user for user in submitted_users if user not in users_with_access]
+#     print(users_without_access)
 
-    successful = list()
-    unsuccessful = list()
+#     successful = list()
+#     unsuccessful = list()
 
-    for user in users_without_access:
-        try:
-            core.grant_read(user)
-            successful.append(user)
-        except HttpError as err:
-            unsuccessful.append((user, err.reason))
+#     for user in users_without_access:
+#         try:
+#             core.grant_read(user)
+#             successful.append(user)
+#         except HttpError as err:
+#             unsuccessful.append((user, err.reason))
 
-    return successful, unsuccessful
+#     return successful, unsuccessful
